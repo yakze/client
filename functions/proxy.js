fe0566368abb193a3952390696f1061746202a9e
yakze <aaahawataame@gmail.com>
2025-02-08 18:55:44 +0900

    Create proxy.js

---
+++ functions/proxy.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const url = event.queryStringParameters.url;
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: 'URL query parameter is required.',
+    };
+  }
+
+  try {
+    // ターゲットのURLに対してHTTPリクエストを送る
+    const response = await fetch(url);
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: `Error fetching the URL: ${error.message}`,
+    };
+  }
+};
