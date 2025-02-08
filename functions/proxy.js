612e0d36155256243d76b1a3c6d9e602443cc119
yakze <aaahawataame@gmail.com>
2025-02-08 19:14:08 +0900

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -2,10 +2,12 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event, context) => {
   const url = event.queryStringParameters.url;
+  console.log('Requested URL:', url);
+
   if (!url) {
     return {
       statusCode: 400,
-      body: 'URL query parameter is required.',
+      body: 'URL parameter is missing.',
     };
   }
 
@@ -13,17 +15,23 @@ exports.handler = async (event, context) => {
     const response = await fetch(url);
     const data = await response.text();
 
+    console.log('Fetched data:', data);
+
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*',  // CORSを許可
+        'Cache-Control': 'no-cache',          // キャッシュを無効化
       },
       body: data,
     };
   } catch (error) {
+    console.error('Error fetching the URL:', error);
+
     return {
       statusCode: 500,
-      body: `Error fetching the URL: ${error.message}`,
+      body: `Error: ${error.message}`,
     };
   }
 };
